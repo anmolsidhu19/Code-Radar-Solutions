af1af5e0196e6637def663e2748f78cb60d1c472
Anmol Singh Sidhu <sidhuanmol0007@gmail.com>
2025-04-06 00:17:31 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     char ch;
-    scanf("%c",&x);
+    scanf("%c",&ch);
 if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
         // Convert to lowercase for easier comparison
         char lower = (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch;
