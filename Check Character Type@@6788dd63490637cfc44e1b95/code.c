55bfd9b801e3e51b80e66ff3501330770c73483e
Anmol Singh Sidhu <sidhuanmol0007@gmail.com>
2025-04-06 00:20:03 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -15,10 +15,11 @@ if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
     else if (ch >= '0' && ch <= '9') {
         printf("Digit");
     }
-    else  {(ch >= 33 && ch <= 47) || (ch >= 58 && ch <= 64) ||
-             (ch >= 91 && ch <= 96) || (ch >= 123 && ch <= 126) 
-        printf("Special Character");
-    }
+    else  {((ch >= 33 && ch <= 47) || (ch >= 58 && ch <= 64) ||
+        (ch >= 91 && ch <= 96) || (ch >= 123 && ch <= 126)) 
+    printf("Special Character");
+}
+
     
     return 0;
 }
